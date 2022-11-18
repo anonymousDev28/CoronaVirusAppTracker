0f8e8dbc2e904f0adf758ca2da598e0290ca9827
anonymousDev28 <92519101+anonymousDev28@users.noreply.github.com>
2022-11-18 23:21:14 +0700

    upload project corona tracker
    learn spring boot

---
+++ coronavirus-tracker/src/main/java/com/learnSpring/coronavirustracker/controllers/HomeController.java
@@ -0,0 +1,27 @@
+package com.learnSpring.coronavirustracker.controllers;
+
+import com.learnSpring.coronavirustracker.models.LocationStats;
+import com.learnSpring.coronavirustracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+    @Autowired
+    CoronaVirusDataService dataService;
+    @GetMapping("/")
+    public String home(Model model){
+        List<LocationStats> allStats = dataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+
+        model.addAttribute("LocationStats",allStats);
+        model.addAttribute("totalReportedCases",totalReportedCases);
+        model.addAttribute("totalNewCases",totalNewCases);
+        return "home";
+    }
+}
